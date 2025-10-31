85a882f3a4472ddd7f1fc4ac635fa4574abeb200
Yazid Bokhari <yabokhar@z1r8p5.42lyon.fr>
2025-10-31 20:36:40 +0100

    ex00 done ?

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -1,6 +1,7 @@
 #include "ScalarConverter.hpp"
 #include <cstdlib>
 #include <cmath>
+#include <limits>
 #define INT_MIN -2147483648
 #define INT_MAX	2147483647
 
@@ -9,12 +10,6 @@ void	ScalarConverter::convert(std::string literal)
 {
 	if (literal.empty())
 		return ;
-	if (literal == "-inff" || literal == "+inff" || literal == "nanf" ||
-        literal == "-inf" || literal == "+inf" || literal == "nan")
-	{
-		handleSpecialValues(literal);
-		return ;
-	}
 	try
 	{
 		switch (detectType(literal))
@@ -44,15 +39,18 @@ void	ScalarConverter::convert(std::string literal)
 				break ;
 			}
 			case (DOUBLE):
+			{
+				char			*end;
+				double const	d = strtod(literal.c_str(), &end);
+
+				if (*end ^ '\0')
+					throw (std::exception());
+				convertDouble(d);
 				break ;
+			}
 			case (INVALID):
-				std::cout << "char: impossible" << std::endl;
-				std::cout << "int: impossible" << std::endl;
-				std::cout << "float: " << literal << "f" << std::endl;
-				std::cout << "double: " << literal << std::endl;
-				break ;
+				std::cout << '\'' + literal + '\'' + " is invalid" << std::endl;
 		}
-		return ;
 	}
 	catch (...)
 	{
@@ -66,16 +64,18 @@ ScalarConverter::Type	ScalarConverter::detectType(const std::string& literal)
 	if (literal.length() == 1 && !isdigit(literal[0]))
 		return (CHAR);
 	
-	char	*end;
-	if (literal[literal.length() - 1] == 'f')
+	bool const	has_dot = (literal.find('.') ^ std::string::npos);
+	bool const	ends_with_f = (literal[literal.length() - 1] == 'f');
+	char		*end;
+
+	if (ends_with_f)
 	{
 		std::string const	withoutF = literal.substr(0, literal.length() - 1);
 		strtod(withoutF.c_str(), &end);
 		if (!*end)
 			return (FLOAT);
 	}
-	if (literal.find('.') == std::string::npos 
-		&& literal.find('f') == std::string::npos)
+	if (!has_dot && !ends_with_f)
 	{
 		long const	l = strtol(literal.c_str(), &end, 10);
 		if (!*end && l >= INT_MIN && l <= INT_MAX)
@@ -113,43 +113,141 @@ void	ScalarConverter::convertInt(int const i)
 void	ScalarConverter::convertFloat(float const f)
 
 {
-	if (std::isnan(f) || std::isinf(f) || f < 0 || f > 127)
+	bool const	is_nan = (f != f);
+	bool const	is_pos_inf = (f == std::numeric_limits<float>::infinity());
+	bool const	is_neg_inf = (f == -std::numeric_limits<float>::infinity());
+
+	if (is_nan || is_pos_inf || is_neg_inf)
+	{
 		std::cout << "char: impossible" << std::endl;
-	else if (f < 32 || f > 126)
-		std::cout << "char: Non displayable" << std::endl;
-	else
-		std::cout << "char: '" << static_cast<char>(f) << "'" << std::endl;
-
-	if (std::isnan(f) || std::isinf(f) || f < INT_MIN /*|| f > INT_MAX*/)
 		std::cout << "int: impossible" << std::endl;
+	}
 	else
-		std::cout << "int: " << static_cast<int>(f) << std::endl;
+	{
+		if (f < 0.0f || f > 127.0f)
+			std::cout << "char: impossible" << std::endl;
+		else if (f < 32.0f || f > 126.0f)
+			std::cout << "char: Non displayable" << std::endl;
+		else
+			std::cout << "char: '" << static_cast<char>(f) << '\'' << std::endl;
 
-	if (f == static_cast<int>(f))
-		std::cout << "float: " << f << ".0f" << std::endl;
+		if (f < static_cast<float>(std::numeric_limits<int>::min()) 
+			|| f > static_cast<float>(std::numeric_limits<int>::max()))
+			std::cout << "int: impossible" << std::endl;
+		else
+			std::cout << "int: " << static_cast<int>(f) << std::endl;
+	}
+
+	if (is_nan)
+		std::cout << "float: nanf" << std::endl;
+	else if (is_pos_inf)
+		std::cout << "float: +inff" << std::endl;
+	else if (is_neg_inf)
+		std::cout << "float: -inff" << std::endl;
 	else
-		std::cout << "float: " << f << 'f' << std::endl;
+	{
+		float		integer_part;
+		float const	fractional_part = modff(f, &integer_part);
+		
+		std::cout << "float: ";
+		if (fractional_part == 0.0f)
+			std::cout << integer_part << ".0f" << std::endl;
+		else
+			std::cout << f << "f" << std::endl;
+	}
+
+	double const	d = static_cast<double>(f);
+	bool const		d_is_nan = (d != d);
+	bool const		d_is_pos_inf = (d == std::numeric_limits<double>::infinity());
+	bool const		d_is_neg_inf = (d == -std::numeric_limits<double>::infinity());
+
+	if (d_is_nan)
+		std::cout << "double: nan" << std::endl;
+	else if (d_is_pos_inf)
+		std::cout << "double: +inf" << std::endl;
+	else if (d_is_neg_inf)
+		std::cout << "double: -inf" << std::endl;
+	else
+	{
+		double			d_integer_part;
+		double const	d_fractional_part = modf(f, &d_integer_part);
+		
+		std::cout << "float: ";
+		if (d_fractional_part == 0.0)
+			std::cout << d_integer_part << ".0" << std::endl;
+		else
+			std::cout << d << std::endl;
+	}
 }
 
-void	ScalarConverter::handleSpecialValues(const std::string& literal)
+void	ScalarConverter::convertDouble(double const d)
 
 {
-	std::cout << "char: impossible" << std::endl;
-	std::cout << "int: impossible" << std::endl;
-	if (literal == "-inff" || literal == "+inff" || literal == "nanf")
+	bool const	is_nan = (d != d);
+	bool const	is_pos_inf = (d == std::numeric_limits<double>::infinity());
+	bool const	is_neg_inf = (d == -std::numeric_limits<double>::infinity());
+
+	if (is_nan || is_pos_inf || is_neg_inf)
 	{
-		std::cout << "float: " << literal << std::endl;
-		std::cout << "double: " << 
-			literal.substr(0, literal.length() - 1) << std::endl;
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
 	}
 	else
 	{
-		std::cout << "float: " << literal << "f" << std::endl;
-		std::cout << "double: " << literal << std::endl;
+		if (d < 0.0 || d > 127.0)
+	 		std::cout << "char: impossible" << std::endl;
+		else if (d < 32.0 || d > 126.0)
+        	std::cout << "char: Non displayable" << std::endl;
+		else
+			std::cout << "char: '" << static_cast<char>(d) << "'" << std::endl;
+
+		if (d < static_cast<double>(std::numeric_limits<int>::min()) || 
+		d > static_cast<double>(std::numeric_limits<int>::max()))
+			std::cout << "int: impossible" << std::endl;
+		else
+			std::cout << "int: " << static_cast<int>(d) << std::endl;
+	}
+
+	float const	f = static_cast<float>(d);
+	bool const	f_is_nan = (f != f);
+	bool const	f_is_pos_inf = (f == std::numeric_limits<float>::infinity());
+	bool const	f_is_neg_inf = (f == -std::numeric_limits<float>::infinity());
+
+	if (f_is_nan)
+		std::cout << "float: nanf" << std::endl;
+	else if (f_is_pos_inf)
+		std::cout << "float: +inff" << std::endl;
+	else if (f_is_neg_inf)
+		std::cout << "float: -inff" << std::endl;
+	else
+	{
+		float 		f_integer_part;
+		float const	f_fractional_part = modff(f, &f_integer_part);
+
+		std::cout << "float: ";
+		if (f_fractional_part == 0.0f)
+    		std::cout << f_integer_part << ".0f" << std::endl;
+		else
+    		std::cout << f << "f" << std::endl;
+    }
+
+	if (is_nan)
+		std::cout << "double: nan" << std::endl;
+	else if (is_pos_inf)
+		std::cout << "double: +inf" << std::endl;
+	else if (is_neg_inf)
+		std::cout << "double: -inf" << std::endl;
+	else
+	{
+		double			integer_part;
+		double const	fractional_part = modf(d, &integer_part);
+		std::cout << "double: ";
+		if (fractional_part == 0.0)
+			std::cout << integer_part << ".0" << std::endl;
+		else
+			std::cout << d << std::endl;
 	}
 }
-
-
 
 
 
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,12 +3,16 @@
 int	main(int argc, const char* argv[])
 
 {
-	if (argc ^ 1)
+	if (argc > 1)
 	{
 		for (int i = 1; i < argc; ++i)
+		{
 			ScalarConverter::convert(argv[i]);
+			if (i ^ argc - 1)
+				std::cout << std::endl;
+		}
 		return (0);
 	}
-	std::cout << "./Conversion_of_scalar_types <string_to_convert>" << std::endl;
+	std::cout << "./Conversion_of_scalar_types <literal>" << std::endl;
 	return (1);
 }
